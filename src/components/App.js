dcebe96d00394a202181c705289f062ad3dc4d9b
garymeehan <garymeehan@outlook.com>
2016-11-22 22:44:15 +0000

    Added AddFishForm and tied in some state

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,31 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.addFish = this.addFish.bind(this);
+
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+
+  }
+
+  addFish(fish) {
+
+    const fishes = {...this.state.fishes};
+
+    const timestamp = Date.now();
+
+    fishes[`fish-${timestamp}`] = fish;
+
+    // es6 below = this.setState({ fishes:fishes });
+    this.setState({ fishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +36,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seefood Market" />
         </div>
         <Order />
-        <Inventory/>
+        <Inventory addFish={ this.addFish } />
       </div>
     )
   }
