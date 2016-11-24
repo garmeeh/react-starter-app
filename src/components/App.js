5419987674a53876111a7843096582e4bca6f469
garymeehan <garymeehan@outlook.com>
2016-11-24 20:11:06 +0000

    Loading sample fish into state

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
 
@@ -9,6 +10,7 @@ class App extends React.Component {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
 
     this.state = {
       fishes: {},
@@ -29,6 +31,12 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  loadSamples(){
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -36,7 +44,8 @@ class App extends React.Component {
           <Header tagline="Fresh Seefood Market" />
         </div>
         <Order />
-        <Inventory addFish={ this.addFish } />
+        <Inventory addFish={ this.addFish }
+          loadSamples={ this.loadSamples } />
       </div>
     )
   }
