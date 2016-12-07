be2efc42d92f5cdf29acef0fcef90f6b4db6c1d3
garymeehan <garymeehan@outlook.com>
2016-12-07 21:25:52 +0000

    Build up order state

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
 
@@ -11,6 +12,7 @@ class App extends React.Component {
 
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
 
     this.state = {
       fishes: {},
@@ -37,11 +39,30 @@ class App extends React.Component {
     });
   }
 
+  addToOrder(key){
+    // copy of current state
+    const order = { ...this.state.order };
+    // update order or add new
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seefood Market" />
+          <ul className="list-of-fishes">
+            {
+              Object
+                .keys(this.state.fishes)
+                .map(key => <Fish key={ key }
+                  index={ key }
+                  details={ this.state.fishes[key] }
+                  addToOrder={ this.addToOrder } />)
+            }
+          </ul>
         </div>
         <Order />
         <Inventory addFish={ this.addFish }
