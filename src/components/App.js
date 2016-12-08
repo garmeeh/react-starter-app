4a9a4ea36c9d44cfac688456730c5687ffe6c5ef
garymeehan <garymeehan@outlook.com>
2016-12-08 21:25:36 +0000

    Live State Editing
    Added inventory with live state editing

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
+    this.updateFish = this.updateFish.bind(this);
 
     this.state = {
       fishes: {},
@@ -60,6 +61,12 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
+    this.setState({fishes});
+  }
+
   loadSamples(){
     this.setState({
       fishes: sampleFishes
@@ -95,8 +102,11 @@ class App extends React.Component {
           fishes={ this.state.fishes }
           order={ this.state.order }
           params={ this.state.params }/>
-        <Inventory addFish={ this.addFish }
-          loadSamples={ this.loadSamples } />
+        <Inventory
+          addFish={ this.addFish }
+          loadSamples={ this.loadSamples }
+          fishes={ this.state.fishes }
+          updateFish={ this.updateFish } />
       </div>
     )
   }
