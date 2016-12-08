2e5c5a9a350e8d51477ce985f21d386f29c5ac84
garymeehan <garymeehan@outlook.com>
2016-12-08 20:17:46 +0000

    Order and Firebase
    - Built up order component
    - Hooked in Firebase using Rebase and React life cycle hooks

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyA8PtHR_4p_Iz00sABYI7AzuR2bp9WoAeE",
+  authDomain: "catch-of-the-day-gary.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-gary.firebaseio.com",
+});
+
+export default base;
